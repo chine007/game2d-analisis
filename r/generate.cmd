28a8b48e80839b844fdb4811e98b9b5fe7fe93b3
Juan Feldman <juanfeldman11@gmail.com>
2013-10-08 15:11:51 -0300

    pom dependencies updated

--- r/generate.cmd
+++ r/generate.cmd
@@ -1,5 +1,5 @@
 @echo off
 rem cmd /c mvn -f ..\features\pom.xml install
 cmd /c mvn -f ..\pom.xml install
-cmd /c mvn compile dependency:copy-dependencies
+cmd /c mvn dependency:copy-dependencies
 pause
